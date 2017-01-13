3983c3eb940fb6371586236f6a5ac84553f17652
zcseguest09 <zcseguest09@w-c02m800lfpf1.ad.unsw.edu.au>
2017-01-13 15:42:53 +1100

    Update turtle draw program

--- draw.py
+++ draw.py
@@ -1,11 +1,11 @@
 import turtle
 
-window = turtle.Screen() # create a new turtle window
-ella = turtle.Turtle() # create a new turtle called Ella
-ella.speed(1) # set speed to 1 so we can see the animation
+window = turtle.Screen()
+ella = turtle.Turtle()
+ella.speed(1)
 
-ella.forward(100) # move ella forward 100
-ella.setheading(90) # set her heading to 90
+ella.forward(100)
+ella.setheading(90)
 ella.forward(100)
 ella.setheading(180)
 ella.forward(100)
