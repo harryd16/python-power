d7c925b9c40681bac467bdcb9fd6f421e0f59d6c
harryd16 <cseguest18@kora18.orchestra.cse.unsw.EDU.AU>
2017-01-13 13:59:29 +1100

    Add programming language popularity graph

---
+++ programming-language_graph.py
@@ -0,0 +1,2 @@
+langugage = ['JavaScript', 'Java', 'Python', 'PHP', 'Ruby', 'C++', 'C', 'Shell', 'C#', 'Objective-C', 'R', 'Go']
+number_of_projects = [323938, 222852, 164852, 138771, 132848, 86 505
